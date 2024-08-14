b2e70b1ae3533ef09113c0590d95b305162c8fe7
judinus10 <judes.20222403@iit.ac.lk>
2024-08-14 11:04:02 +0530

    Initialising Methods

---
+++ c.py
@@ -0,0 +1,15 @@
+class conductBook :
+    def __init__(self):
+        pass
+    def add(self,name=None,phoneNumber=None,email=None):
+        pass
+    def delete(self,phonenumber=None):
+        pass
+    def edit(self,name=None,phoneNumber=None,email=None):
+        pass
+    def view(self):
+        pass
+    def search(self,query=None,sortField=None):
+        pass
+    def console(self):
+        pass
